06da80d3f358bfdf5606fce3be373f25f05919f5
Cameron <xwhalex@icloud.com>
2020-01-21 16:44:15 +0800

    Nav Bar Changes

--- src/App.js
+++ src/App.js
@@ -2,41 +2,42 @@ import React from 'react';
 import './App.css';
 import { AppBar, Toolbar, Container, TextField, Button } from '@material-ui/core';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import Example from "./NavBar"
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return (
 
-    <React.Fragment>
-      <AppBar position="fixed" color="#0d47a1">
-        <Toolbar>{"歡迎來到我的網站demo"}</Toolbar>
-      </AppBar>
-      <Toolbar />
+      <React.Fragment>
+        <Example></Example>
 
-      <Container>
-        <hgroup>
-          <h1>請登入</h1>
-        </hgroup>
-        <form>
-          <div class="group">
-            <TextField id="outlined-basic" label="帳號" variant="outlined" fullWidth="true" />
-          </div>
-          <div class="group">
-            <TextField id="outlined-basic" label="密碼" variant="outlined" fullWidth="true" type="password" />
-          </div>
-          <Button
-            variant="contained"
-            color="default"
-            endIcon={<KeyboardArrowRightIcon />}
-          >
-            登入
+        <Container>
+
+          <hgroup>
+            <h1>請登入</h1>
+          </hgroup>
+          <form>
+            <div class="group">
+              <TextField id="outlined-basic" label="帳號" variant="outlined" fullWidth="true" />
+            </div>
+            <div class="group">
+              <TextField id="outlined-basic" label="密碼" variant="outlined" fullWidth="true" type="password" />
+            </div>
+            <Button
+              variant="contained"
+              color="default"
+              endIcon={<KeyboardArrowRightIcon />}
+              onClick={() => { alert('hi'); }}
+            >
+              登入
       </Button>
-        </form>
+          </form>
 
 
-      </Container>
-    </React.Fragment>
+        </Container>
+      </React.Fragment>
 
-  );
+    );
+  }
 }
-
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+} from 'reactstrap';
+
+const Example = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">致勝先師</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/secondpage/">關於我們</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                排課規劃
+              </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    瀏覽課表
+                </DropdownItem>
+                                <DropdownItem>
+                                    編輯課表
+                </DropdownItem>
+
+                            </DropdownMenu>
+                        </UncontrolledDropdown><UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                學生資訊
+              </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    新增
+                </DropdownItem>
+                                <DropdownItem>
+                                    修改
+                </DropdownItem>
+                                <DropdownItem>
+                                    刪除
+                </DropdownItem>
+                                <DropdownItem>
+                                    查詢
+                </DropdownItem>
+
+                            </DropdownMenu>
+                        </UncontrolledDropdown><UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                學生報表
+              </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    每月授課計畫表
+                </DropdownItem>
+                                <DropdownItem>
+                                    每月類別堂數表
+                </DropdownItem>
+                                <DropdownItem>
+                                    每月科別堂數表
+                </DropdownItem>
+                                <DropdownItem>
+                                    每月星期堂數表
+                </DropdownItem>
+                                <DropdownItem>
+                                    每月個別學生堂數表
+                </DropdownItem>
+
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                教師資訊
+              </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    新增
+                </DropdownItem>
+                                <DropdownItem>
+                                    修改
+                </DropdownItem>
+                                <DropdownItem>
+                                    刪除
+                </DropdownItem>
+                                <DropdownItem>
+                                    查詢
+                </DropdownItem>
+                                <DropdownItem>
+                                    排班
+                </DropdownItem>
+
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+export default Example;
